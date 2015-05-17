890d04a8187a9401ff89c3ca0c7f3e7bd9fdc754
Kobe Osei-Akoto <kobeosei-akoto@Kobes-MacBook-Pro.local>
2015-05-17 15:16:18 -0400

    Different API endpoint

--- mashup.js
+++ mashup.js
@@ -19,38 +19,17 @@ if (navigator.geolocation) {
 
   setTimeout(function() {
 
-    $.when(
       $.ajax({
-        url: 'https://api.instagram.com/v1/locations/search?lat=' + lat +'&lng=' + lon + '&client_id=ce95cb4e56c146c994457b48a839f6a8',
+        url: 'https://api.instagram.com/v1/media/search?lat=' + lat + '&lng=' + lon + '&client_id=ce95cb4e56c146c994457b48a839f6a8',
         dataType: 'jsonp',
         success: function(result){
-          // for (var i = 0; i < 1; i++){
-            link = result.data[2].id;
-        }
-      })                  
-    ).then(function() {
-      console.log(link);
-      $.ajax({
-        url: 'https://api.instagram.com/v1/locations/' +link+ '/media/recent?client_id=ce95cb4e56c146c994457b48a839f6a8',
-        dataType: 'jsonp',
-        success: function(result){
-          console.log(result);
-          for (var i = 0; i < result.data.length; i++){
-
-             var url = result.data[i].images.thumbnail.url;
-              console.log(url);
-              $('body').append('<img src="' + url + '"/>');
-          //   console.log(result[i]);
-          //   console.log("it worked!");
-
-          //   // console.log(url);
-          //   // $('body').append('<img src="' + url + '"/>');
+          for (var i = 0; i < result.data.length; i++) {
+              var url = result.data[i].images.thumbnail.url;
+             $('body').append('<img src="' + url + '"/>');
           }
         }
-      });    
-    }); 
-
-  }, 2000);
+      });                  
+  }, 3000);
 }
 
 })();
